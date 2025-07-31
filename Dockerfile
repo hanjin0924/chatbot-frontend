548827fadc1ce90719ee8b465afbac913725d129
jiyeon <journiyoon@gmail.com>
2025-07-31 13:26:47 +0900

    Rollback build mode

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@
 FROM node:18-alpine AS builder
 WORKDIR /app
 COPY package.json package-lock.json ./
-RUN npm install
+RUN npm ci
+
 COPY . .
 RUN npm run build
 
@@ -10,10 +11,11 @@ RUN npm run build
 FROM node:18-alpine AS runner
 ENV NODE_ENV=production
 WORKDIR /app
-COPY --from=builder /app/.next ./.next
-COPY --from=builder /app/public ./public
+
+COPY --from=builder /app/.next/standalone ./
+COPY --from=builder /app/.next/static ./.next/static
 COPY --from=builder /app/package.json ./package.json
 COPY --from=builder /app/node_modules ./node_modules
 EXPOSE 3000
 
-CMD ["node", ".next/standalone/server.js"]
+CMD ["node", ".next/server.js"]
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -40,19 +40,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarFooter>
         <SidebarMenu>
          
-          <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild>
-              <Link href="https://github.com/assistant-ui/assistant-ui" target="_blank">
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <Github className="size-4" />
-                </div>
-                <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">GitHub</span>
-                  <span className="">View Source</span>
-                </div>
-              </Link>
-            </SidebarMenuButton>
-            
+          <SidebarMenuItem>           
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarFooter>
