8a2dffc414293314dfae55ab26e42f8fd854d439
jihoone <jungjihoons@naver.com>
2025-08-12 21:02:13 +0900

    Update tooltip default location

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    position: "top-right", // bottom-left(기본), bottom-right, top-left, top-right
+  },
 };
 
 export default nextConfig;
